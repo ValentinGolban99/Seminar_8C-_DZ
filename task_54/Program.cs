95095902fe5be50136cfbd3ce433953c3dbe8ff9
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-09-17 14:39:33 +0300

    task 54

---
+++ task_54/Program.cs
@@ -0,0 +1,56 @@
+﻿void FillArray(int [,] array, int startNumber = 0, int finishNumber = 10)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    Random random = new Random();
+    finishNumber++;
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++) 
+        {
+            array[i, j] = random.Next(startNumber, finishNumber);
+        }
+    }
+}
+void PrintArray(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t"); 
+        }
+    Console.WriteLine();
+    }
+}
+void Zadacha54()
+{
+    // Задача 54: Задайте двумерный массив. Напишите программу, которая
+    // упорядочит по убыванию элементы каждой строки двумерного массива.
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int columns = random.Next(4, 8);
+    int[,] array = new int[rows, columns];
+    FillArray(array, -100, 100);
+    PrintArray(array);
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < array.GetLength(1) - 1; z++)
+        {
+            if (array[i, z] < array[i, z + 1])
+            {
+                int temp = 0;
+                temp = array[i, z];
+                array[i, z] = array[i, z + 1];
+                array[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(array);
+}
+Zadacha54();
