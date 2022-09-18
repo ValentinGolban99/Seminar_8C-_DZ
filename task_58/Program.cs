5380d8f98cda3fce73e6dcba30824abdcaaeb5cc
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-09-18 09:10:11 +0300

    Последняя задача далась очень туго

--- task_58/Program.cs
+++ task_58/Program.cs
@@ -1,4 +1,10 @@
-﻿void FillArray(int [,] array, int startNumber = 0, int finishNumber = 10)
+﻿int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+void FillArray(int [,] array, int startNumber = 0, int finishNumber = 10)
 {
     int rows = array.GetLength(0);
     int columns = array.GetLength(1);
@@ -23,19 +29,38 @@ void PrintArray(int[,] inputMatrix)
     Console.WriteLine();
     }
 }
-void Zadacha58()
+void MultiplyMatrix(int[,] matrixA, int[,] matrixB, int[,] matrixC)
 {
-    // Задача 58: Задайте две матрицы. Напишите программу, которая будет
-    // находить произведение двух матриц.
-    Random random = new Random();
-    int rows = random.Next(3, 5);
-    int columns = random.Next(3, 5);
-    int[,] array = new int[rows, columns];
-    FillArray(array);
-    PrintArray(array);
-    int[,] matrixA = new int[size, size];
-    int[,] matrixB = new int[size, size];
-    FillArrayRandomNumbers(matrixA);
-    FillArrayRandomNumbers(matrixB);
-    int[,] matrixC = new int[size, size];
+  for (int i = 0; i < matrixC.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrixC.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < matrixA.GetLength(1); k++)
+      {
+        sum += matrixA[i,k] * matrixB[k,j];
+      }
+      matrixC[i,j] = sum;
+    }
+  }
 }
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет
+// находить произведение двух матриц.
+Random random = new Random();
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int[,] matrixA = new int[m, n];
+int[,] matrixB = new int[n, p];
+int[,] matrixC = new int[m, p];
+FillArray(matrixA);
+PrintArray(matrixA);
+Console.WriteLine();
+FillArray(matrixB);
+PrintArray(matrixB);
+Console.WriteLine();
+MultiplyMatrix(matrixA, matrixB, matrixC);
+PrintArray(matrixC);
+
